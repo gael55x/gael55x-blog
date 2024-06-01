a7aea46e5e94fcc055dd29b43a6a1e36f8eb6fee
gael55x <gailleamolong5@gmail.com>
2024-06-01 15:34:06 +0800

    Added post components -> post item and all post

--- gael55x-blog/components/layout/logo.jsx
+++ gael55x-blog/components/layout/logo.jsx
@@ -1,7 +1,7 @@
 import classes from './logo.module.css';
 
 function Logo() {
-  return <div className={classes.logo}>Gaille's Developer Blog</div>;
+  return <div className={classes.logo}>Gaille' Developer Blog</div>;
 }
 
 export default Logo;
