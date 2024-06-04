d60e5232860c3e503819893e709215b868fd437c
gael55x <gailleamolong5@gmail.com>
2024-06-04 15:05:26 +0800

    Notification UI fixes

--- gael55x-blog/pages/_document.jsx
+++ gael55x-blog/pages/_document.jsx
@@ -6,9 +6,9 @@ class MyDocument extends Document {
       <Html lang='en'>
         <Head />
         <body>
+          <div id="notifications"></div>
           <Main />
           <NextScript />
-          <div id="notifications"></div>
         </body>
       </Html>
     );
